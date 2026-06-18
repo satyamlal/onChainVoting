baed9ec3395029b8da576e94cbb02aeafd92c43a
Test <ssatyamlal@gmail.com>
2026-06-18 14:46:28 +0530

    update initialize_candidate()

--- programs/on_chain_voting/src/lib.rs
+++ programs/on_chain_voting/src/lib.rs
@@ -6,11 +6,19 @@ declare_id!("GUTMvs4uirowQBJstmRsiHkvKL2UdS6F3FTTeq29tqoC");
 pub mod on_chain_voting {
     use super::*;
 
-    pub fn init_poll(ctx: Context<InitPoll>, _poll_id: u64) -> Result<()> {
-        // initialize::handler(ctx, poll_id)
+    pub fn init_poll(ctx: Context<InitPoll>, _poll_id: u64, start: u64, end: u64, name: String, description: String) -> Result<()> {
         let poll = &mut ctx.accounts.poll_account;
-        poll.poll_option_index = 0;
+        poll.poll_name = name;
+        poll.poll_description = description;
+        poll.poll_start = start;
+        poll.poll_end = end;
         Ok(())
+    }
+
+    pub fn initialize_candidate(ctx: Context<InitializeCandidate>, _poll_id: u64, candidate: String) -> Result<()> {
+        ctx.accounts.candidate_account.candidate_name = candidate;
+        ctx.accounts.poll_account.poll_option_index += 1;
+        OK(())
     }
 }
 
@@ -26,6 +34,23 @@ pub struct InitPoll<'info> {
         space = 8 + PollAccount::INIT_SPACE,
         seeds = [b"poll".as_ref(), poll_id.to_le_bytes().as_ref()],
         bump        
+    )]
+    pub poll_account: Account<'info, PollAccount>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(poll_id: u64, candidate: String)]
+pub struct InitCandidate<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(
+        init,
+        payer = signer,
+        space = 8 + PollAccount::INIT_SPACE,
+        seeds = [b"poll".as_ref(), poll_id.to_le_bytes().as_ref()],
+        bump
     )]
     pub poll_account: Account<'info, PollAccount>,
     pub system_program: Program<'info, System>,
