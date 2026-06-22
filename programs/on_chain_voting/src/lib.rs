42521baa93a908c6a20cc0e8ddfb1af0c99f9d98
Test <ssatyamlal@gmail.com>
2026-06-22 11:45:27 +0530

    feat: add poll_account account validation with seeds and bump to voting app

--- programs/on_chain_voting/src/lib.rs
+++ programs/on_chain_voting/src/lib.rs
@@ -6,19 +6,20 @@ declare_id!("GUTMvs4uirowQBJstmRsiHkvKL2UdS6F3FTTeq29tqoC");
 pub mod on_chain_voting {
     use super::*;
 
-    pub fn init_poll(ctx: Context<InitPoll>, _poll_id: u64, start: u64, end: u64, name: String, description: String) -> Result<()> {
-        let poll = &mut ctx.accounts.poll_account;
+    pub fn init_poll(ctx: Context<InitPoll>, _poll_id: u64, poll_voting_start: u64, poll_voting_end: u64, name: String, description: String) -> Result<()> {
+        // let poll = &mut ctx.accounts.poll_account;
+        let mut poll = ctx.accounts.poll_account;
         poll.poll_name = name;
         poll.poll_description = description;
-        poll.poll_start = start;
-        poll.poll_end = end;
+        poll.poll_voting_start = poll_voting_start;
+        poll.poll_voting_end = poll_voting_end;
         Ok(())
     }
 
-    pub fn initialize_candidate(ctx: Context<InitializeCandidate>, _poll_id: u64, candidate: String) -> Result<()> {
+    pub fn initialize_candidate(ctx: Context<InitCandidate>, _poll_id: u64, candidate: String) -> Result<()> {
         ctx.accounts.candidate_account.candidate_name = candidate;
         ctx.accounts.poll_account.poll_option_index += 1;
-        OK(())
+        Ok(())
     }
 }
 
@@ -46,13 +47,20 @@ pub struct InitCandidate<'info> {
     pub signer: Signer<'info>,
 
     #[account(
+        mut,
+        seeds = [b"poll".as_ref(), poll_id.to_le_bytes().as_ref()],
+        bump
+    )]
+    pub poll_account: Account<'info, PollAccount>,
+
+    #[account(
         init,
         payer = signer,
         space = 8 + PollAccount::INIT_SPACE,
         seeds = [b"poll".as_ref(), poll_id.to_le_bytes().as_ref()],
         bump
     )]
-    pub poll_account: Account<'info, PollAccount>,
+    pub candidate_account: Account<'info, CandidateAccount>,
     pub system_program: Program<'info, System>,
 }
 
@@ -64,8 +72,8 @@ pub struct PollAccount {
 
     #[max_len(280)]
     pub poll_description: String,
-    pub poll_start: u64,
-    pub poll_end: u64,
+    pub poll_voting_start: u64,
+    pub poll_voting_end: u64,
     pub poll_option_index: u64
 }
 
